9af8b5cea6ab1f84e407ab3139061df37c6e863f
Yanis Mahé <yanis.mahe@etu.iut-tsle3.fr>
2024-03-04 12:16:22 +0100

    Terminado

--- kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
+++ kata_code_smell/src/main/java/fr/mutation/kata2/Exercice.java
@@ -7,21 +7,20 @@ import java.util.Map;
 
 public class Exercice {
 
-    public Map<Integer, String> f1(int a1, List<Integer> a2) {
-        HashMap<Integer, String> a7 = new HashMap<Integer, String>();
-        int a5 = 0;
-        StringBuilder a9 = new StringBuilder();
-        for (int a3 = 0; a3 < a2.size() && a3 < a1; a3++) {
-            int a6 = a2.get(a3);
-            int a10 = a6 - 1;
-            if (a6 >= 0 && (a10) < 20) {
-                a9.append(a6).append(" ");
-            } else {
-                a5++;
-            }
-        }
-        a9.append("\n");
-        a7.put(a5, a9.toString());
-        return a7;
+    public Map<Integer, String> f1(int limit, List<Integer> integerList) {
+        HashMap<Integer, String> result = new HashMap<Integer, String>();
+        String resultString = integerList.stream()
+        	.limit(limit)
+        	.filter(n -> (n >= 0 && n < 21))
+        	.map(Object::toString)
+        	.reduce("", (string, element) -> string + element + " ");
+        int outOfBoundaryCount = (int) integerList.stream()
+        	.limit(limit)
+        	.filter(n -> !(n >= 0 && n <21))
+        	.count();
+        	
+        resultString += "\n";
+        result.put(outOfBoundaryCount, resultString);
+        return result;
     }
 }
--- kata_code_smell/src/test/java/fr/mutation/kata2/ExerciceTests.java
+++ kata_code_smell/src/test/java/fr/mutation/kata2/ExerciceTests.java
@@ -11,125 +11,72 @@ import org.junit.jupiter.api.Test;
 
 public class ExerciceTests {
 
-
-
-    // !!! pour comprendre comment marche une Map/HashMap en java, lisez ceci:  https://www.data-transitionnumerique.com/java-hashmap/
-
     @Test
     public void listeVide() {
-        //given
         final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
-        //when
+
         Map<Integer, String> actual = exercice.f1(1, listeEntiers);
-        //ASSERT
+
         Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
-        //dans le 1er element de la Map<Int, String> on trouve la clé qui est une entier qui contient le nombre d'entiers positifs trouvés
-        // eton trouve dans la valeur <String> de cette map, la chaine de caractère
-
-
         Assertions.assertThat(dualResult.getKey()).isZero();
-
-
-        //2e verification:  que contient la chaine de caractère?
         Assertions.assertThat(dualResult.getValue()).isEqualTo("\n");
     }
 
     @Test
-    public void listeAvecUnEntierPositif() {
-        //given
+    public void listeAvecDeuxEntiersPositifs() {
         final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
-        listeEntiers.add(8);
-        //when
-        Map<Integer, String> actual = exercice.f1(1, listeEntiers);
-        //then
-        Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
-        Assertions.assertThat(dualResult.getKey()).isZero();
-        //2e verification:  que contient la chaine de caractère?
-        Assertions.assertThat(dualResult.getValue()).isEqualTo("8 \n");
-    }
+        listeEntiers.add(0);
+        listeEntiers.add(20);
 
-    // @Test
-    public void listeAvecDeuxEntiersNegatifs() {
-        //given
-        final Exercice exercice = new Exercice();
-        final List<Integer> listeEntiers = new ArrayList<>();
-        listeEntiers.add(-1);
-        listeEntiers.add(-2);
-        //when
         Map<Integer, String> actual = exercice.f1(2, listeEntiers);
-        //then
+
         Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
-        Assertions.assertThat(dualResult.getKey()).isEqualTo(2);
-        //2e verification:  que contient la chaine de caractère?
-        Assertions.assertThat(dualResult.getValue()).isEqualTo("\n");
+        Assertions.assertThat(dualResult.getKey()).isZero();
+        Assertions.assertThat(dualResult.getValue()).isEqualTo("0 20 \n");
     }
 
     @Test
-    public void listeAvecUnPositifEtUnNegatif() {  // ce test est-il utile?
-        //given  / ARRANGE
+    public void listeAvecElementsNonPrisEnCompte() {
         final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
-        listeEntiers.add(-9);
+        listeEntiers.add(4);
         listeEntiers.add(9);
-        //when / ACT
+        listeEntiers.add(14);
+        listeEntiers.add(-14);
+
         Map<Integer, String> actual = exercice.f1(2, listeEntiers);
-        //then / ASSERT
+
         Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
-        Assertions.assertThat(dualResult.getKey()).isEqualTo( (2 - 1));
-        Assertions.assertThat(dualResult.getValue()).isEqualTo("9 \n");
+        Assertions.assertThat(dualResult.getKey()).isZero();
+        Assertions.assertThat(dualResult.getValue()).isEqualTo("4 9 \n");
     }
 
     @Test
-    public void listeAvecTroisPositifsEtUnNeg() {  // règle des 3 A
-        //given  / ARRANGE
-        final Exercice exercice = new Exercice();
+    public void listeAvecUnEntierSuperieur20() {
+    	final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
-        listeEntiers.add(4);   //1
-        listeEntiers.add(9);    //2
-        listeEntiers.add(8);    //3
-        listeEntiers.add(-8);   //4
-        listeEntiers.add(-9);   // non pris en compte
+        listeEntiers.add(21);
+        
+        Map<Integer, String> actual = exercice.f1(1, listeEntiers);
 
-        //when / ACT
-        Map<Integer, String> actual = exercice.f1(4, listeEntiers);
-        //then / ASSERT
         Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
         Assertions.assertThat(dualResult.getKey()).isEqualTo(1);
-        Assertions.assertThat(dualResult.getValue()).isEqualTo("4 9 8 \n");
+        Assertions.assertThat(dualResult.getValue()).isEqualTo("\n");
     }
     
-    // lancer les tests avec du code coverage
-    // puis lancer les tests avec PItest: mvn test -P pitest
-
-    //  EXERCICE 1:  Il reste trop de mutants en vie:  les corriger avec des nouveaux tests
-    //  important: 1 nouveau test pour tuer 1 mutant à la fois
-
-    // EXERCICE 2: monter pitest au niveau STRONGER
-
-
     @Test
-    public void listeAvecTroisPositifsLimiteA2() {  //  EXERCICE 2:  Il reste un mutant en vie:  le corriger ici
-        //given  / ARRANGE
-        final Exercice exercice = new Exercice();
+    public void listeAvecUnEntierNegatif() {
+    	final Exercice exercice = new Exercice();
         final List<Integer> listeEntiers = new ArrayList<>();
-        listeEntiers.add(1);
-        listeEntiers.add(9);
-        listeEntiers.add(8);
+        listeEntiers.add(-1);
+        
+        Map<Integer, String> actual = exercice.f1(1, listeEntiers);
 
-        //when / ACT
-        Map<Integer, String> actual = exercice.f1(2, listeEntiers);
-        //then / ASSERT
         Entry<Integer, String> dualResult = actual.entrySet().iterator().next();
-
+        Assertions.assertThat(dualResult.getKey()).isEqualTo(1);
+        Assertions.assertThat(dualResult.getValue()).isEqualTo("\n");
     }
 
-
-
-/*   EXERCICE 3 : une fois obtenu un Test Strength de 100%, trouvez quels sont les tests qui ne servent à rien...
-    */
-
-    // EXERCICE 4: finir le refactoring en nommant correctement les variables
-    // et en supprimant les lignes inutiles ou stupides
 }
